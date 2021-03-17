79c2584bbcd8c17d6918b19d975b1856ba43e3ba
esellers25 <erin.e.sellers@gmail.com>
2021-03-16 20:25:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(routine="go to the office") {
+    return `This Monday, I will ${routine}.`
+}
+
+function wrapAdjective(string="*") {
+    return function(element="special") {
+        return `You are ${string}${element}${string}!`;
+    }
+}
